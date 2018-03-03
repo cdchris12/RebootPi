d2078d5bf39203bf8d14da6c917c0e0c57c578cc
Chris Davis <cdchris12@gmail.com>
2018-03-02 22:29:39 -0600

    Update RebootServer and add config
    Removed global var for miner objects, and rewrote the miner class a bit.
    Should be a bit more robust now. Also added a config file to remove some
    hardcoded url dependencies.

--- RebootServer.py
+++ RebootServer.py
@@ -1,35 +1,41 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
+
+# Try loading the config file and die if not found
 try:
-    import RPi.GPIO as GPIO
-except RuntimeError:
-    print("Error importing RPi.GPIO! This is probably because you need superuser privileges. You can achieve this by using 'sudo' to run your script.")
+    os.stat("config.json")
+except Exception:
+    print "Config file missing!!!"
+    return(1)
+else:
+    with open('config.json', 'rb') as infile:
+        config = json.load(infile.read())
+    # End with
 # End try/except block
 
+import RPi.GPIO as GPIO
+from time import sleep
 import requests
 import arrow
-import time
 
 # Listing of all GPIO pins we can use, 17 total
 GPIO_pins = [7, 11, 12, 13, 15, 16, 18, 22, 29, 31, 32, 33, 35, 36, 37, 38, 40]
 
-# Listing of all miners
-miner_nums = ["00", "01", "02", "03", "04", "05", "06"]
-
-# List of miner objects
-miner_objs = []
+# Global list to hold our miner objects
+miners = []
 
 class miner:
-    def __init__ (self, miner_num, earl, io_num):
+    def __init__ (self, name, earl, io_num, port):
         self.io_num = io_num
         self.earl = earl
-        self.miner_num = miner_num
+        self.name = name
+        self.port = port
     # End def
 
-    def health_check (self):
+    def healthCheck (self):
         # Returns True/False based on a good or bad health check.
         try:
-            r = requests.get(self.earl, timeout=10)
+            r = requests.get("%s:%s" % (self.earl, self.port), timeout=10)
         except requests.exceptions.Timeout:
             return False
         else:
@@ -43,22 +49,19 @@ class miner:
 
     def change (self):
         GPIO.output(self.io_num, GPIO.HIGH)
-        time.sleep(0.5)
+        sleep(0.5)
         GPIO.output(self.io_num, GPIO.LOW)
     # End def
 
     def reboot (self):
         GPIO.output(self.io_num, GPIO.HIGH)
-        time.sleep(10)
+        sleep(10)
         GPIO.output(self.io_num, GPIO.LOW)
         self.change()
     # End def
 # End class
 
-
 def Setup():
-    print("Starting up the monitor at %s..." % getTime())
-
     # Set the GPIO pins to use the board numbering scheme
     GPIO.setmode(GPIO.BOARD)
 
@@ -68,41 +71,40 @@ def Setup():
     # Setup all GPIO pins as output devices
     GPIO.setup(GPIO_pins, GPIO.OUT, initial=GPIO.LOW)
 
-    print("Set up the GPIO pins!")
-
     # Create the miner objects
-    getMiners()
+    miners = getMiners()
 
-    print("Set up the miner objects!")
+    # Return the created miner object list
+    return miners
 # End def
 
 def Cleanup():
     # Release all listed GPIO pins
     GPIO.cleanup(GPIO_pins)
-
-    print("\nCleaned up the GPIO pin configs!")
 # End def
 
-def RunServer():
-    for miner in miner_objs:
-        if not miner.health_check:
+def RunServer(miners):
+    # First, we want to make sure all the miners are at least turned on
+    for miner in miners:
+        if not miner.healthCheck():
             miner.change()
-            print("Started miner %s!" % (miner.miner_num))
+            #print("Started miner %s!" % (miner.name))
         else:
-            print("Miner %s already running!" % (miner.miner_num))
+            #print("Miner %s already running!" % (miner.name))
+            pass
         # End if/else block
     try:
         while True:
-            print("Sleeping for 300 seconds...")
-            time.sleep(300)
+            #print("Sleeping for 300 seconds...")
+            sleep(300)
 
-            print("Starting regular checks at %s..." % getTime())
-            for miner in miner_objs:
-                if not miner.health_check:
-                    print("Miner %s wasn't responding, so we're rebooting it!" % miner.miner_num)
+            #print("Starting regular checks at %s..." % getTime())
+            for miner in miners:
+                if not miner.healthCheck():
+                    #print("Miner %s wasn't responding, so we're rebooting it!" % miner.name)
                     miner.reboot()
                 else:
-                    print("miner %s's health check passed!" % miner.miner_num)
+                    #print("miner %s's health check passed!" % miner.name)
                 # End if/else block
             # End for
         # End while
@@ -116,25 +118,25 @@ def getTime():
 # End def
 
 def getMiners():
-    global miner_objs
+    global miners
 
     ret = []
-    for num in miner_nums:
-        ret.append( miner(num, earl="http://interstate-towing.ddns.net:70"+num, io_num=GPIO_pins[int(num)]))
+    for item in config:
+        ret.append( miner(name=item['name'], earl="%s:%s" % (item['url'], item['port']), io_num=GPIO_pins[item['io_num']]))
     # End for
-
-    miner_objs = [f for f in ret]
 
     return ret
 # End def
 
 def main():
-    Setup()
-    RunServer()
-    Cleanup()
-
-    print("Exiting at %s, goodbye!" % getTime())
-    return(0)
+    try:
+        miners = Setup()
+        RunServer(miners)
+    except Exception:
+        Cleanup()
+    finally:
+        return(0)
+    # End try/except block
 # End def
 
 if __name__ == "__main__":
