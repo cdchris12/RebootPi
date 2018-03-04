46b4b7e5540b252a785c27110e0ff591f9305a0e
Chris Davis <cdchris12@gmail.com>
2018-03-04 17:08:52 -0600

    So. Many. Typos

--- RebootServer.py
+++ RebootServer.py
@@ -94,8 +94,8 @@ def Setup(config):
 
     # Setup all GPIO pins as either input or output devices
     for miner in miners:
-        GPIO.setup(miner['io_out_num'], GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(miner['io_in_num'], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(miner.io_out_num, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(miner.io_in_num, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     # End for
 
     # Return the created miner object list
